cece9298bc595fa613d323b4b07b203071119dfb
zero-trace-official <zerotrace-support@proton.me>
2025-02-27 09:15:03 -0800

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cors());
+app.use(cookieParser());
 
 // API Routes
 const adminRoutes = require('./routes/adminRoutes');
